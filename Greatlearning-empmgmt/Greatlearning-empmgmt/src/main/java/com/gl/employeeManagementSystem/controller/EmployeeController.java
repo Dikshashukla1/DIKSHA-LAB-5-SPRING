cab54a0a6fd67ecb23b550f08d6b0ffc4991052c
Dikshashukla1 <106049102+Dikshashukla1@users.noreply.github.com>
2023-04-06 13:12:46 +0530

    Update EmployeeController.java

--- Greatlearning-empmgmt/Greatlearning-empmgmt/src/main/java/com/gl/employeeManagementSystem/controller/EmployeeController.java
+++ Greatlearning-empmgmt/Greatlearning-empmgmt/src/main/java/com/gl/employeeManagementSystem/controller/EmployeeController.java
@@ -46,12 +46,28 @@ public class EmployeeController {
 		empService.deleteEmployee(id);
 		return "redirect:/employees/list";
 	}
-
+	
 	@GetMapping("/update/{id}")
-	public String updateEmp(Model model, @PathVariable int id) {
-		Employee employee = empService.getEmployee(id);
-		model.addAttribute("employee", employee);
-		return "employee";
+	public String editemployeeForm(@PathVariable int id, Model model) {
+		model.addAttribute("employee", empService.getEmployee(id));
+		return "edit_employee";
+	}
+
+	@PostMapping("/{id}")
+	public String updateemployee(@PathVariable int id, @ModelAttribute("employee") Employee employee, Model model) {
+
+		// get employee from database by id
+
+		Employee existingEmployee = empService.getEmployee(id);
+		existingEmployee.setId(id);
+		existingEmployee.setFirstName(employee.getFirstName());
+		existingEmployee.setLastName(employee.getLastName());
+		existingEmployee.setEmail(employee.getEmail());
+
+		// save updated employee object
+		empService.updateEmployee(existingEmployee);
+
+		return "redirect:/employees/list";
 	}
 
 }
